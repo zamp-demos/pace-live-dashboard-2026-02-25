d991f809ca7c86a88d575655e95824c61960ad39
Pace <pace@zamp.ai>
2026-02-26 08:58:06 +0000

    Remove debug test-key endpoint

--- api/test-key.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.ANTHROPIC_API_KEY || 'NOT_SET';
-  res.json({
-    keyLength: key.length,
-    keyPrefix: key.substring(0, 14),
-    keySuffix: key.substring(key.length - 10),
-    keySet: key !== 'NOT_SET'
-  });
-}
